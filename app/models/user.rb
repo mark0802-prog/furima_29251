2529f92e13420f19e6c694db9db6b792226fb2e1
mark0802-prog <hayabua.deu.horn@live.jp>
2020-08-20 20:40:49 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  zennkaku = /\A[ぁ-んァ-ン一-龥]/
+  zennkaku_katakana = /\A[ァ-ヶー－]+\z/
+  alphanumeric_mixture = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name, format: { with: zennkaku, message: 'is invalid. Input full-width characters.' }
+    validates :last_name, format: { with: zennkaku, message: 'is invalid. Input full-width characters.' }
+    validates :first_name_kana, format: { with: zennkaku_katakana, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_kana, format: { with: zennkaku_katakana, message: 'is invalid. Input full-width katakana characters.' }
     validates :birth_date
   end
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+  validates :password, format: { with: alphanumeric_mixture,
                                  message: 'is invalid. Input half-width alphanumeric mixture.' }
 end
