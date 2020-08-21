2b5f95c95e20978ce144389d8cfa3b620cd549f1
mark0802-prog <hayabua.deu.horn@live.jp>
2020-08-21 12:00:55 +0900

    ストロングパラメタの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category, :salse_status, :shipping_fee_status, :prefecture, :scheduled_delivery, :price, :image).merge(user_id: current_user.id)
   end
 end
