d5ea8db6ffeeb5173d2f2145de4360666e6744a8
mark0802-prog <hayabua.deu.horn@live.jp>
2020-08-21 11:41:21 +0900

    itemsテーブルの作成

---
+++ db/migrate/20200821021417_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.text :name, null: false
+      t.text :info, null: false
+      t.integer :category, null: false
+      t.integer :salse_status, null: false
+      t.integer :shipping_fee_status, null: false
+      t.integer :prefecture, null: false
+      t.integer :scheduled_delivery, null: false
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
