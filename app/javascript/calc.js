e58cdd2d20db110cedfc56f1af2dd184daec38e3
mark0802-prog <hayabua.deu.horn@live.jp>
2020-08-23 00:05:28 +0900

    非同期で販売価格、手数料を表示する機能を改善

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -3,6 +3,15 @@ const calc = () => {
   const add_tax_price_box = document.getElementById('add-tax-price')
   const profit_box = document.getElementById('profit')
 
+  if (price_box == null) {
+    return null
+  }
+  else if (price_box.getAttribute('data_load') != null) {
+    return null
+  }
+
+  price_box.setAttribute('data_load', 'true')
+
   price_box.addEventListener("input", (e) => {
     let price = parseInt(e.target.value)
     let add_tax_price = parseInt(price * 0.1)
@@ -17,4 +26,4 @@ const calc = () => {
   })
 }
 
-addEventListener("load", calc)
+setInterval(calc, 1000)
