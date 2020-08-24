c724b18f46d7f3553df1f430a79205e43f92beaa
mark0802-prog <hayabua.deu.horn@live.jp>
2020-08-24 12:07:34 +0900

    商品購入ビューの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_user_noteq_current_user?
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_user_noteq_current_user?
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if @item.user.id == current_user.id
+  end
 end
