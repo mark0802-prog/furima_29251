554b3ee9655c3d16ccd1bbcb4dbafcecc18be77a
mark0802-prog <hayabua.deu.horn@live.jp>
2020-08-31 21:13:33 +0900

    複数画像投稿機能の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,35 +1,53 @@
 const preview = () => {
-  const imgUpload = document.getElementById("item-preview")
+  const itemPreview = document.getElementById("item-preview")
   const itemImage = document.getElementById("item-image")
 
   if (itemImage == null) {
     return null
   }
 
-  // const sakujyo = () => {
-  //   const imageContent = document.getElementById("item-preview")
-  //   if (imageContent) {
-  //     imageContent.remove()
-  //   }
-  // }
-
-  itemImage.addEventListener("change", (e) => {
-    const imageContent = document.querySelector("#item-preview img")
-    if (imageContent) {
-      imageContent.remove()
-    }
-
-    const file = e.target.files[0]
-    const blob = URL.createObjectURL(file)
-
+  const createImageHTML = (blob) => {
     const imageElement = document.createElement("div")
+    imageElement.setAttribute("id", "image-element")
+    let imageElementNum = document.querySelectorAll("#image-element").length
 
     const blobImage = document.createElement("img")
     blobImage.setAttribute("src", blob)
     blobImage.setAttribute("class", "item-preview")
 
+    const inputHTML = document.createElement("input")
+    inputHTML.setAttribute("id", `item_image_${imageElementNum}`)
+    inputHTML.setAttribute("name", "item[images][]")
+    inputHTML.setAttribute("type", "file")
+
     imageElement.appendChild(blobImage)
-    imgUpload.appendChild(imageElement)
+    itemPreview.appendChild(imageElement)
+    itemPreview.appendChild(inputHTML)
+
+    inputHTML.addEventListener("change", (e) => {
+      // const imageContent = document.querySelector("#item-preview img")
+      // if (imageContent) {
+      //   imageContent.remove()
+      // }
+  
+      file = e.target.files[0]
+      blob = URL.createObjectURL(file)
+
+      createImageHTML(blob)
+    })
+  }
+
+  itemImage.addEventListener("change", (e) => {
+    // const imageContent = document.querySelector("#item-preview img")
+    // if (imageContent) {
+    //   imageContent.remove()
+    // }
+
+    const file = e.target.files[0]
+    const blob = URL.createObjectURL(file)
+
+    createImageHTML(blob)
+
   })
 }
 
