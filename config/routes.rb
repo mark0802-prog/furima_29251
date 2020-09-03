fb1e3b7d7072ba26adaba945031b66f192890c45
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-03 20:30:26 +0900

    マイページからカード登録ページへの遷移を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'cards/index'
   root 'items#index'
-  devise_for :users, controllers: {registrations: 'users/registrations'}
-  resources :users, only: [:show]
+  devise_for :users, controllers: {registrations: 'users/registrations'} do 
+    resources :cards, only: [:index, :create]
+  end
   resources :items do
     resources :orders, only: [:index, :create]
   end
