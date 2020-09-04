5f6b31f233c245340cb41c3094ae929729f7c405
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-04 12:53:37 +0900

    カード登録機能の実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,5 +4,26 @@ class CardsController < ApplicationController
   end
 
   def create
+    customer_create
+    card = Card.new(card_params)
+    if card.save
+      redirect_to root_path
+    else
+      render "index"
+    end
+  end
+
+  private
+
+  def customer_create
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    @customer = Payjp::Customer.create(
+      description: "test",
+      card: params[:card_token]
+    )
+  end
+
+  def card_params
+    params.permit(:card_token).merge(customer_token: @customer.id).merge(user_id: current_user.id)
   end
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,17 +13,17 @@ const pay = () => {
     const formData = new FormData(formResult)
 
     const card = {
-      number: formData.get("order_address[number]"),
-      exp_month: formData.get("order_address[exp_month]"),
-      exp_year: `20${formData.get("order_address[exp_year]")}`,
-      cvc: formData.get("order_address[cvc]")
+      number: formData.get("card[number]"),
+      exp_month: formData.get("card[exp_month]"),
+      exp_year: `20${formData.get("card[exp_year]")}`,
+      cvc: formData.get("card[cvc]")
     }
 
     Payjp.createToken(card, (status, response) => {
       if (status === 200)  {
         const token = response.id
         const renderDom = document.getElementById('charge-form')
-        const tokenObj = `<input value=${token} type="hidden" name="token">`
+        const tokenObj = `<input value=${token} type="hidden" name="card_token">`
         renderDom.insertAdjacentHTML('beforeend', tokenObj)
 
         document.getElementById('card-number').removeAttribute("name")
