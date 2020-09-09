d6fc4f24f2ac3b91d1a23ba2fff965fcd1d49ad9
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-09 12:37:35 +0900

    モデルのテストコード修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    token { Faker::Internet.password }
     postal_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city { Faker::Address.city }
